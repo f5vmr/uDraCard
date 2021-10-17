8127b62f736a073c9d4be1ad40cb1facfc3e4223
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2021-10-17 17:48:05 +0100

    Update uDraProg.py

--- uDraProg.py
+++ uDraProg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-#IMPORT UTILITAIRES
+#IMPORT CONFIGURATION
 import serial
 import re
 import parametres as s
@@ -12,18 +12,18 @@ baud = '9600'
 ser = serial.Serial(serport, baud, timeout=2)
 
 
-#CONTROLE SAISIE DES FREQUENCES
+#CVERIFYING FREQUENCIES
 def validate(freq):
 	r = re.compile('\d{3}[\s.]\d{4}')
 	s = str(freq)
 	if len(s) ==8 and r.match(s):
-		print(freq+"->FREQUENCE CONFORME")
+		print(freq+"->FREQUENCY CONFORMEs")
 	else:
-		print('\x1b[7;37;41m'+freq+"->ERREUR DE SAISIE DE FREQUENCE, MERCI DE REFAIRE LA CONFIGURATION"+'\x1b[0m')
+		print('\x1b[7;37;41m'+freq+"->ERROR INPUTTING FREQUENCY, TRY TO REDO THE CONFIGURATION"+'\x1b[0m')
 		exit()
 
 def connect():
-	#CONNECTION AU DRA SI=0 OK SI=1 PAS OK
+	#CONNECTION to DRA SI=0 OK SI=1 Not OK
 	ser.write(b'AT+DMOCONNECT\r\n')
 	output = ser.readline()
 	print('Opening port: ' + ser.name)
@@ -31,10 +31,10 @@ def connect():
 	if output.decode("utf-8")!="":
 		print('reponse (0=OK): ' + output.decode("utf-8"))
 	else:
-		print('\x1b[7;37;41m'+"VERIFIER LA CONNEXION AVEC LE DRA/SA818! ( switch 2 et 3 en ON)"+'\x1b[0m')
+		print('\x1b[7;37;41m'+"VERIFIE THE CONNECTION WITH DRA/SA818! ( switch 2 and 3 are ON)"+'\x1b[0m')
 		exit()
 
-#DEFINITON DES FONCTIONS
+#DEFINITON OF the FUNCTIONS
 def volume():
 	volume = 'AT+DMOSETVOLUME={}\r\n'.format(s.volumelevel)
 	ser.write(volume.encode())
@@ -42,42 +42,42 @@ def volume():
 	print('Reponse (0=OK) (1=KO) : ' + output.decode("utf-8"))
 	
 	if output.decode("utf-8")!="":
-		print('reponse (0=OK): ' + output.decode("utf-8"))
+		print('response (0=OK): ' + output.decode("utf-8"))
 	else:
-		print('\x1b[7;37;41m'+"VERIFIER LA CONNEXION AVEC LE DRA/SA818! ( switch 2 et 3 en ON)"+'\x1b[0m')
+		print('\x1b[7;37;41m'+"VERIFY THE CONNECTION WITH THE DRA/SA818! ( switch 2 and 3 are ON)"+'\x1b[0m')
 		exit()
 
-	print('Le volume est maintenant a : ' +str(s.volumelevel))
+	print('The volume is now at : ' +str(s.volumelevel))
 	print("-+-+-+-+-+-+-+-+-+-+-+-+-+-")
 
 def filters():
 	filter = 'AT+SETFILTER={},{},{}\r\n'.format(s.filterpre, s.highpass, s.lowpass)
 	ser.write(filter.encode())
 	output = ser.readline()
-	print('Envoi commande filtres vers le DRA ;) ')
-	print('Reponse du DRA (0=OK) (1=KO) : ' + output.decode("utf-8"))
+	print('Send the filter commands to the DRA ;) ')
+	print('Response from DRA (0=OK) (1=KO) : ' + output.decode("utf-8"))
 	
 	if output.decode("utf-8")!="":
-		print('reponse (0=OK): ' + output.decode("utf-8"))
+		print('response (0=OK): ' + output.decode("utf-8"))
 	else:
-		print('\x1b[7;37;41m'+"VERIFIER LA CONNEXION AVEC LE DRA/SA818! ( switch 2 et 3 en ON)"+'\x1b[0m')
+		print('\x1b[7;37;41m'+"VERIFY THE CONNECTION WITH THE DRA/SA818! ( switch 2 and 3 are ON)"+'\x1b[0m')
 		exit()
 
-	print('Reglage des filtres DRA en cours...')
+	print('Regulations of the DRA filter underway...')
 	print('                                      pre/dehamphasis          highpass          lowpass')
-	print('Les filtres sont maintenant a :       ' +str(s.filterpre)+'                        ' +str(s.highpass)+'             ' +str(s.lowpass))
+	print('The filters are now at :       ' +str(s.filterpre)+'                        ' +str(s.highpass)+'             ' +str(s.lowpass))
 	
 def config():
 	config = 'AT+DMOSETGROUP={},{},{},{},{},{}\r\n'.format(s.channelspace, s.txfreq, s.rxfreq, s.txctcss, s.squelch, s.rxctcss)
 	ser.write(config.encode())
 	print(config)
 	output = ser.readline()
-	print('Envoi commande 0=12.5kHz, 1=25kHz: '+str(s.channelspace))
-	print('Envoi frequence TX: '+str(s.txfreq))
-	print('Envoi frequence RX: '+str(s.rxfreq))
-	print('Envoi CTCSS TX: '+str(s.txctcss))
-	print('Envoi CTCSS RX: '+str(s.rxctcss))
-	print('Envoi squelch: '+str(s.squelch))
+	print('Send command 0=12.5kHz, 1=25kHz: '+str(s.channelspace))
+	print('Send frequency TX: '+str(s.txfreq))
+	print('Send frequency RX: '+str(s.rxfreq))
+	print('Send CTCSS TX: '+str(s.txctcss))
+	print('Send CTCSS RX: '+str(s.rxctcss))
+	print('Send squelch: '+str(s.squelch))
 
 validate(s.txfreq)
 validate(s.rxfreq)
